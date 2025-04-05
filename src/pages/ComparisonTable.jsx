539561ff5eca0a8c79214739d7619a68a49cc7a5
workashutosh <mashutoshh@gmail.com>
2025-04-05 23:19:28 +0530

    new design changes

--- src/pages/ComparisonTable.jsx
+++ src/pages/ComparisonTable.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const ComparisonTable = () => {
   return (
-    <div className=" mx-auto px-6 py-16 h-fit items-center bg-gray-800 text-white">
+    <div className=" mx-auto px-10 py-16 h-fit items-center bg-gray-800 text-white">
       <h1 className="text-3xl md:text-4xl font-bold text-left mb-6">
-       <span className='text-blue-500'>Logicense</span>   Custom vs. Off-the-Shelf Software – A Clear Winner Emerges
+       <span className='text-transparent bg-clip-text bg-gradient-to-r from-blue-300 to-blue-600'>Logicense</span>   Custom vs. Off-the-Shelf Software – A Clear Winner Emerges
       </h1>
       <p className="py-4 mb-8 text-lg text-left md:text-xl">
         At Logicense, we believe crafting business solutions—whether CRMs, CLMs, landing pages, marketing tools, animations, models, VFX, or more—should be a tailored experience, not a one-size-fits-all guesswork. We follow a client-driven innovation process to transform your vision into a solution that elevates your business. Here’s why custom solutions from Logicense stand out:
@@ -77,6 +77,16 @@ const ComparisonTable = () => {
           </tbody>
         </table>
       </div>
+      <a
+  href="/contact"
+  class="group inline-flex text-center py-8 items-center justify-center px-4  text-blue-600 font-medium  transition-all duration-200 hover:shadow-lg relative overflow-hidden"
+>
+  Let's Build a Better One Together!
+  <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 ml-2" viewBox="0 0 20 20" fill="currentColor">
+    <path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 10l-6-6a1 1 0 010-1.414z" clip-rule="evenodd" />
+  </svg>
+  <span class="absolute bottom-0 left-0 w-full h-[2px] bg-blue-600 transform scale-x-0 group-hover:scale-x-100 transition-transform duration-200 ease-in-out"></span>
+</a>
     </div>
   );
 };
--- src/pages/Why.jsx
+++ src/pages/Why.jsx
@@ -9,7 +9,7 @@ const Why = () => {
         <h2 className='text-4xl lg:text-5xl text-left font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-300 to-blue-600 mb-6 leading-tight'>
             Why you'll love us?
         </h2>
-        <p className='text-base lg:text-lg text-gray-100 mb-6 '>
+        <p className='text-[25px]  text-gray-100 mb-6 '>
             We’re 10x thinkers and change makers, driving extreme-value innovation through human-centric design and emerging tech, defying the traditional. With a track record of serving multiple businesses globally, collaborating with VC firms, startups, SMEs, and governments, while also developing our own GenAI products – we’re obsessed with building high-impact products, ready for scale.
         </p>
       </div>
